af8e43aab816d25b39d520b4fd07f41dd8e7316f
Nocoa <nocoa@gmx.com>
2018-01-27 17:02:58 -0500

    Add pickup logic
    charges, dropping, picking up, etc.

--- Transmission/Assets/Scripts/GunPickup.cs
+++ Transmission/Assets/Scripts/GunPickup.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class GunPickup : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int charge;
+
+    // Use this for initialization
+    void Start() {}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update() {}
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            CombatActions playerObj = other.gameObject.GetComponent<CombatActions>();
+            if (playerObj.HasWeapon())
+                playerObj.ResetWeapon();
+            playerObj.hasGun = true;
+            playerObj.charge = charge;
+            Destroy(gameObject);
+        }
+    }
 }
--- Transmission/Assets/Scripts/MeleePickup.cs
+++ Transmission/Assets/Scripts/MeleePickup.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class MeleePickup : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int charge;
+
+    // Use this for initialization
+    void Start() {}
+
+    // Update is called once per frame
+    void Update() {}
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            CombatActions playerObj = other.gameObject.GetComponent<CombatActions>();
+            if (playerObj.HasWeapon())
+                playerObj.ResetWeapon();
+            playerObj.hasHammer = true;
+            playerObj.charge = charge;
+            Destroy(gameObject);
+        }
+    }
 }
